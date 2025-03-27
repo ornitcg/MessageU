3fa889399dbd8ead222567377c8e49d61c399cfb
Ornit Cohen Gindi <ornit.cgindi@gmail.com>
2025-03-27 20:31:56 +0200

    deleted tests

--- Test/test.cpp
+++
@@ -1,8 +0,0 @@
-#include "pch.h"
-#include <gtest/gtest.h>
-#include "RequestInfo.h"
-
-TEST(TestDisplayMenu, displayMenu) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
-}
--- test1/test.cpp
+++
@@ -1,8 +0,0 @@
-#include "pch.h"
-#include <gtest/gtest.h>
-#include "../MessageUClient/RequestInfo.h"
-
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
-}
